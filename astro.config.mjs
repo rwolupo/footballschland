37573c71dc365e024cf28e8b82e5ec1bf1721d10
rwolupo <duengel@gmail.com>
2026-03-28 16:02:45 +0100

    Add @sanity/astro integration with studio at /studio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import sanity from '@sanity/astro';
 
 export default defineConfig({
-  site: 'https://footballschland.netlify.app',
-  integrations: [
-    tailwind(),
-    sitemap(),
-  ],
+    site: 'https://footballschland.netlify.app',
+    integrations: [
+          tailwind(),
+          sitemap(),
+          sanity({
+                  projectId: 'k31tvjv8',
+                  dataset: 'production',
+                  useCdn: false,
+                  studioBasePath: '/studio',
+          }),
+        ],
 });
