df60c53caa07efbb11d08a56fa6322080aa5b1dd
rwolupo <duengel@gmail.com>
2026-03-28 16:21:34 +0100

    Add Netlify adapter and output:hybrid for SSR studio route

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,20 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import sanity from '@sanity/astro';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-    site: 'https://footballschland.netlify.app',
-    integrations: [
-          tailwind(),
-          sitemap(),
-          sanity({
-                  projectId: 'k31tvjv8',
-                  dataset: 'production',
-                  useCdn: false,
-                  studioBasePath: '/studio',
-          }),
-        ],
+  site: 'https://footballschland.netlify.app',
+  output: 'hybrid',
+  adapter: netlify(),
+  integrations: [
+    tailwind(),
+    sitemap(),
+    sanity({
+      projectId: 'k31tvjv8',
+      dataset: 'production',
+      useCdn: false,
+      studioBasePath: '/studio',
+    }),
+  ],
 });
